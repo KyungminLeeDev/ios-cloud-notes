9303fa0557f6a6d52794f66ef4c9f4c0a5d4329e
zziro95 <opers7878@gmail.com>
2021-03-02 16:17:45 +0900

    Feat #15 - CoreDataManager 객체 생성

---
+++ CloudNotes/CloudNotes/CoreData/CoreDataManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataManager.swift
+//  CloudNotes
+//
+//  Created by Jinho Choi on 2021/03/02.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    static let shared: CoreDataManager = CoreDataManager()
+    
+    private init() {}
+}
